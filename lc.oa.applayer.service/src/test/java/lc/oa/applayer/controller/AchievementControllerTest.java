cef6cfe8c12fa4b27b2927d92835387ac700054d
zhangxiaolin <zxl_198313>
2020-09-15 10:10:20 +0800

    update config server test

---
+++ lc.oa.applayer.service/src/test/java/lc/oa/applayer/controller/AchievementControllerTest.java
@@ -0,0 +1,15 @@
+package lc.oa.applayer.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AchievementControllerTest {
+    @Test
+    public void test(){
+        if (true && false && true){
+            System.out.println("1");
+        }
+        System.out.println("2");
+    }
+}
