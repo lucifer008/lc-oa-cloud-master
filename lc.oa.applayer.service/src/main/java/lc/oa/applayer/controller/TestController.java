89aa85547a376c5f6312640155dd6652134bdc4f
zhangxiaolin <zxl_198313>
2020-09-17 15:24:46 +0800

    gateway router eureka配置

--- lc.oa.applayer.service/src/main/java/lc/oa/applayer/controller/TestController.java
+++ lc.oa.applayer.service/src/main/java/lc/oa/applayer/controller/TestController.java
@@ -1,5 +1,6 @@
 package lc.oa.applayer.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,10 @@ import java.util.Date;
 @RequestMapping("/test")
 public class TestController {
     private SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm.sss");
+    @Value("${server.port}")
+    private String port;
     @RequestMapping("/hello")
     public String hello(){
-        return simpleDateFormat.format(new Date())+"--->hello";
+        return simpleDateFormat.format(new Date())+"--->hello--->端口:"+port;
     }
 }
