94a98c093523067cc40006972846a1ae4afb2a3a
zhangxiaolin <zxl_198313>
2020-09-17 10:51:51 +0800

    nocas配置中心spring boot 2.0版本+gateway

--- gateway-server/src/main/java/com/lecar/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/lecar/GatewayServerApplication.java
@@ -8,6 +8,34 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+  *
+  * @Description:
+ * SpringCloud Gateway 是 Spring Cloud 的一个全新项目，该项目是基于 Spring 5.0，Spring Boot 2.0 和 Project Reactor 等技术开发的网关，
+ * 它旨在为微服务架构提供一种简单有效的统一的 API 路由管理方式。
+ * SpringCloud Gateway 作为 Spring Cloud 生态系统中的网关，目标是替代 Zuul，在Spring Cloud 2.0以上版本中，
+ * 没有对新版本的Zuul 2.0以上最新高性能版本进行集成，仍然还是使用的Zuul 2.0之前的非Reactor模式的老版本。而为了提升网关的性能，
+ * SpringCloud Gateway是基于WebFlux框架实现的，而WebFlux框架底层则使用了高性能的Reactor模式通信框架Netty。
+ * Spring Cloud Gateway 的目标，不仅提供统一的路由方式，并且基于 Filter 链的方式提供了网关基本的功能，例如：安全，监控/指标，和限流。
+ *
+ * SpringCloud官方，对SpringCloud Gateway 特征介绍如下：
+ *
+ * （1）基于 Spring Framework 5，Project Reactor 和 Spring Boot 2.0
+ *
+ * （2）集成 Hystrix 断路器
+ *
+ * （3）集成 Spring Cloud DiscoveryClient
+ *
+ * （4）Predicates 和 Filters 作用于特定路由，易于编写的 Predicates 和 Filters
+ *
+ * （5）具备一些网关的高级功能：动态路由、限流、路径重写
+  * @Author:         lucifer
+  * @CreateDate:     2020/9/17 10:48
+  * @UpdateUser:
+  * @UpdateDate:
+  * @UpdateRemark:
+  * @Version:        1.0
+ */
 @EnableDiscoveryClient
 @EnableFeignClients
 @RestController
--- gateway-server/src/main/java/com/lecar/config/RouterConfig.java
+++ gateway-server/src/main/java/com/lecar/config/RouterConfig.java
@@ -4,6 +4,11 @@ package com.lecar.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,20 +17,47 @@ import org.springframework.stereotype.Component;
  * @author limengyang
  * create 2020-07-29
  **/
-@Setter
-@Getter
-@Component
+@Configuration
 public class RouterConfig {
-
-    @Value("${spring.cloud.nacos.server-addr}")
-    private String serverAddr;
-
-    @Value("${nacos.router.dataId:gateway-router.json}")
-    private String dataId;
-
-    @Value("${nacos.router.group:DEFAULT_GROUP}")
-    private String group;
-
-    @Value("${spring.cloud.nacos.config.namespace}")
-    private String namespace;
+    /**
+     * @method  路由
+     * @description
+     * http://localhost:8080/get
+     *
+     * @date: 2020/9/17 10:37
+     * @author: lucifer
+     * @param
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        //@formatter:off
+        return builder.routes()
+                .route("path_route", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .route("rewrite_route", r -> r.host("*.rewrite.org")
+                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)",
+                                "/${segment}"))
+                        .uri("http://httpbin.org"))
+                .route("hystrix_route", r -> r.host("*.hystrix.org")
+                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
+                        .uri("http://httpbin.org"))
+                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
+                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
+                        .uri("http://httpbin.org"))
+                .route("limit_route", r -> r
+                        .host("*.limited.org").and().path("/anything/**")
+                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
+                        .uri("http://httpbin.org"))
+                .route("websocket_route", r -> r.path("/echo")
+                        .uri("ws://localhost:9000"))
+                .build();
+        //@formatter:on
+    }
+    @Bean
+    RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1, 2);
+    }
 }
