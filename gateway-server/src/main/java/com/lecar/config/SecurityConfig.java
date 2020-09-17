e6a20c04e4c307ce71e4a52356773062ba84db48
zhangxiaolin <zxl_198313>
2020-09-17 10:35:21 +0800

    nocas配置中心spring boot 2.0版本+gateway

--- gateway-server/src/main/java/com/lecar/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/lecar/config/SecurityConfig.java
@@ -1,6 +1,12 @@
 package com.lecar.config;
 
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,9 +15,50 @@ import org.springframework.context.annotation.Configuration;
 //@EnableWebSecurity
 public class SecurityConfig //extends WebSecurityConfigurerAdapter
 {
-//    @Override
+    //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
 //       // super.configure(http);
 //        http.authorizeRequests().anyRequest().permitAll().and().logout().permitAll();
 //    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        //@formatter:off
+        return builder.routes()
+                .route("path_route", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .route("rewrite_route", r -> r.host("*.rewrite.org")
+                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)",
+                                "/${segment}"))
+                        .uri("http://httpbin.org"))
+                .route("hystrix_route", r -> r.host("*.hystrix.org")
+                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
+                        .uri("http://httpbin.org"))
+                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
+                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
+                        .uri("http://httpbin.org"))
+                .route("limit_route", r -> r
+                        .host("*.limited.org").and().path("/anything/**")
+                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
+                        .uri("http://httpbin.org"))
+                .route("websocket_route", r -> r.path("/echo")
+                        .uri("ws://localhost:9000"))
+                .build();
+        //@formatter:on
+    }
+    @Bean
+    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+        return http.httpBasic().and()
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/anything/**").authenticated()
+                .anyExchange().permitAll()
+                .and()
+                .build();
+    }
+    @Bean
+    RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1, 2);
+    }
 }
